ada534ce59dcdc2eb096f43021e10bc2b5ec0c8b
AYUSH BADGUJAR <badgujarabhimanyu@gmail.com>
2022-11-30 11:29:38 +0530

    if - else- syntax
    if/else/synatx

---
+++ C/Class Work/syntax if.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ //Condition : it evalutes to boolean value.
+ 				// i.e ture(1) or false(0).
+ 	void main()
+ 	{
+	 
+ 	
+ 	int A;
+ 	
+ 	printf("Enter value  A : ");
+ 	scanf("%d",& a);
+ 	
+ 	if (a>0)
+ 	{
+ 		printf("\nIt is +ve No.");
+	}
+	else
+		printf("\nIt is -ve No");
+	}
---
+++ C/if.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+	void main()
+{
+	int a;
+	printf("\nEnter A :",a);
+	scanf("%d",&a);
+	
+		if(a%2==0)
+		
+		{
+			printf("\n %d Value is even",a);
+			
+		}
+		else 
+		
+		{
+	printf("\n%d Value is odd ",a);
+}
+	
+	
+}
